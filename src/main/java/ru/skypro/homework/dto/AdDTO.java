1978f4812270ca25639356f0f9f967beb53fbab8
Konstantin <kdanilov1911@gmail.com>
2023-08-03 00:50:23 +0300

    AdController, AdDTO, ListAdsDTO, Advertisement, CreateOrUpdateAdDTO added

---
+++ src/main/java/ru/skypro/homework/dto/AdDTO.java
@@ -0,0 +1,15 @@
+package ru.skypro.homework.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
+
+@Data
+@AllArgsConstructor
+public class AdDTO {
+    private int ID;
+    private int idUser;
+    private String image;
+    private int price;
+    private String title;
+}
